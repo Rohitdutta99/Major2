6b44bf20ea4c58f1a80788ea1942d60d7aa44d06
Rohit Dutta <rohitdutta2.21@nshm.edu.in>
2024-03-24 15:20:43 +0530

    second time commit

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -2,14 +2,17 @@ import React from "react";
 import Banner from "./Banner";
 import imgLinks from "../imgLinks";
 import Gap from "./Gap_ele";
+import Gym_listings from "./Gym_listings";
+import Subheadings from "./Subheadings";
 
 function Body(props){
     return (<div className="body_div">
         <Gap />
         <Banner key={imgLinks[0].key} id={imgLinks[0].key} img={imgLinks[0].imageURL} />
         <Gap />
+        <Subheadings text="Featured Gyms"/>
         <Gap />
-        <Gap />
+        <Gym_listings />
         <Gap />
     </div>);
 }
